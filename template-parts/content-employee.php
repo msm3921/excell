b128c931b3adb5101506348a1a026b68b99e43f4
matt mueller <matt@matts-MacBook-Pro.local>
2019-09-05 11:21:54 -0400

    employee custom post types appears working. will add another employee later to test

--- template-parts/content-employee.php
+++ template-parts/content-employee.php
@@ -1,17 +1,37 @@
 <?php 
 ///Advanced Custom Fields
-$employee_image		                = get_field('employee_image');
-$employee_name		                = get_field('employee_name');
 $employee_title		                = get_field('employee_title');
-$employee_description		        = get_field('employee_description');
  ?>
 
 <div id="employee">
-            <img src="<?php echo $employee_image['url']; ?>" alt="<?php echo $employee_image['alt']; ?>">
-            <div class=employee-content>
-                <h5><?php echo $employee_name?></h5>
-                <h6><?php echo $employee_title?></h6>
-                <p><?php echo truncate_string($employee_description,150, ' ...'); ?></p>
-            </div>
-            <button><p>More on <?php echo truncate_string($employee_name,1, ' >>'); ?></p></button>
+    <?php 
+        $loop = new WP_Query( array( 'post_type' => 'employee', 'orderby' => 'post_id', 'order' => 'ASC' ) );
+    ?>
+
+        <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+            <?php 
+                    $image = get_field('employee_image');
+                    $name  = get_field('employee_name');
+                    $title = get_field('employee_title');
+                    $description = get_field('employee_description');
+                    if( !empty($image) ): ?>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+            
+
+                    <div class=employee-content>
+                        <h5><?php echo $name?></h5>
+                        <h6><?php echo $title?></h6>
+                        <p><?php echo truncate_string($description,150, ' ...'); ?></p>
+                    </div>
+
+                    <button><p>More on <?php echo truncate_string($name,1, ' >>'); ?></p></button>
+            <?php endif; ?> 
+    <?php endwhile; wp_reset_query(); ?>
 </div> <!--employee-->
+
+
+<?php 
+$image = get_field('image');
+if( !empty($image) ): ?>
+  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+<?php endif; ?>
